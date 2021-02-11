59026955c3870a141a70f555f3a83452e85c9f3b
Martin Wilhelmi <mnin@mnin.de>
2021-02-11 12:59:44 +0100

    Bump version up to 1.2

--- src/Coinbase.lua
+++ src/Coinbase.lua
@@ -26,7 +26,7 @@
 -- SOFTWARE.
 
 WebBanking {
-  version = 1.11,
+  version = 1.2,
   url = "https://api.coinbase.com",
   description = "Fetch balances from Coinbase API and list them as securities",
   services = { "Coinbase Account" },
